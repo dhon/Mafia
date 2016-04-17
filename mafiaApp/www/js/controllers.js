0f110101363c47fdbdc8a13a7c202c82bdc8940e
Andrew Chang <changster109@gmail.com>
2016-04-17 15:44:28 -0400

    Prototype Model

--- mafiaApp/www/js/controllers.js
+++ mafiaApp/www/js/controllers.js
@@ -1,6 +1,42 @@
 angular.module('app.controllers', [])
   
-.controller('pageCtrl', function($scope) {
+.controller('page1Ctrl', function($scope) {
+
+})
+   
+.controller('addPlayersCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('statsCtrl', function($scope) {
+
+})
+   
+.controller('pendingCtrl', function($scope) {
+
+})
+   
+.controller('gameNightCtrl', function($scope) {
+
+})
+   
+.controller('gameDayCtrl', function($scope) {
+
+})
+   
+.controller('voteCtrl', function($scope) {
+
+})
+   
+.controller('rulesCtrl', function($scope) {
 
 })
  
--- mafiaApp/www/js/routes.js
+++ mafiaApp/www/js/routes.js
@@ -10,10 +10,64 @@ angular.module('app.routes', [])
     
   
 
-      .state('page', {
+      .state('page1', {
     url: '/page1',
-    templateUrl: 'templates/page.html',
-    controller: 'pageCtrl'
+    templateUrl: 'templates/page1.html',
+    controller: 'page1Ctrl'
+  })
+
+  .state('addPlayers', {
+    url: '/page3',
+    templateUrl: 'templates/addPlayers.html',
+    controller: 'addPlayersCtrl'
+  })
+
+  .state('signup', {
+    url: '/page5',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('login', {
+    url: '/page6',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('stats', {
+    url: '/page7',
+    templateUrl: 'templates/stats.html',
+    controller: 'statsCtrl'
+  })
+
+  .state('pending', {
+    url: '/page8',
+    templateUrl: 'templates/pending.html',
+    controller: 'pendingCtrl'
+  })
+
+  .state('gameNight', {
+    url: '/page9',
+    templateUrl: 'templates/gameNight.html',
+    controller: 'gameNightCtrl'
+  })
+
+  .state('gameDay', {
+    url: '/page11',
+    templateUrl: 'templates/gameDay.html',
+    controller: 'gameDayCtrl'
+  })
+
+  .state('vote', {
+    url: '/page10',
+    templateUrl: 'templates/vote.html',
+    controller: 'voteCtrl'
+  })
+
+  .state('rules', {
+    url: '/page12',
+    templateUrl: 'templates/rules.html',
+    controller: 'rulesCtrl'
   })
 
 $urlRouterProvider.otherwise('/page1')
