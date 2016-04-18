887a7c8a1a0ed9500ac99d21b7ae7f948e7fb105
ssowersby <ssowersby@umass.edu>
2016-04-17 20:17:14 -0400

    v

--- mafiaApp/www/js/controllers.js
+++ mafiaApp/www/js/controllers.js
@@ -1,46 +1,46 @@
 angular.module('app.controllers', [])
-  
+
 .controller('page1Ctrl', function($scope, $ionicNavBarDelegate) {
-$ionicNavBarDelegate.showBackButton(false);
+	$ionicNavBarDelegate.showBackButton(false);
 })
-   
+
 .controller('addPlayersCtrl', function($scope, $ionicNavBarDelegate) {
-$ionicNavBarDelegate.showBackButton(false);
+	$ionicNavBarDelegate.showBackButton(false);
 })
-   
+
 .controller('signupCtrl', function($scope, $ionicNavBarDelegate) {
-$ionicNavBarDelegate.showBackButton(false);
+	$ionicNavBarDelegate.showBackButton(false);
 })
-   
+
 .controller('loginCtrl', function($scope, $ionicNavBarDelegate) {
-$ionicNavBarDelegate.showBackButton(false);
+	$ionicNavBarDelegate.showBackButton(false);
 })
-   
+
 .controller('statsCtrl', function($scope, $ionicNavBarDelegate) {
-$ionicNavBarDelegate.showBackButton(false);
+	$ionicNavBarDelegate.showBackButton(false);
+	$scope.user = BlankFactory.getUser();
 })
-   
+
 .controller('pendingCtrl', function($scope, $ionicNavBarDelegate) {
-$ionicNavBarDelegate.showBackButton(false);
+	$ionicNavBarDelegate.showBackButton(false);
 })
-   
+
 .controller('gameNightCtrl', function($scope, $ionicNavBarDelegate) {
-$ionicNavBarDelegate.showBackButton(false);
+	$ionicNavBarDelegate.showBackButton(false);
 })
-   
+
 .controller('gameDayCtrl', function($scope, $ionicNavBarDelegate) {
-$ionicNavBarDelegate.showBackButton(false);
+	$ionicNavBarDelegate.showBackButton(false);
 })
-   
+
 .controller('voteCtrl', function($scope, $ionicNavBarDelegate) {
-$ionicNavBarDelegate.showBackButton(false);
+	$ionicNavBarDelegate.showBackButton(false);
 })
-   
+
 .controller('rulesCtrl', function($scope, $ionicNavBarDelegate) {
-$ionicNavBarDelegate.showBackButton(false);
+	$ionicNavBarDelegate.showBackButton(false);
 })
-   
+
 .controller('resultsCtrl', function($scope, $ionicNavBarDelegate) {
-$ionicNavBarDelegate.showBackButton(false);
+	$ionicNavBarDelegate.showBackButton(false);
 })
- 
--- mafiaApp/www/js/services.js
+++ mafiaApp/www/js/services.js
@@ -1,7 +1,14 @@
 angular.module('app.services', [])
 
 .factory('BlankFactory', [function(){
-
+	return{
+		var user={};
+		user.games = 10;
+		user.victories = 5;
+		getUser: function(){
+			return user;
+		}
+	}
 }])
 
 .service('BlankService', [function(){
