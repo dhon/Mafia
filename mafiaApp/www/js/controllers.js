4f26cd531cdaf800326e61a30f13b9fe2b955a3e
Andrew Chang <changster10@gmail.com>
2016-04-18 17:50:53 -0400

    added input/output display for html
    its amazing

--- mafiaApp/www/js/controllers.js
+++ mafiaApp/www/js/controllers.js
@@ -4,43 +4,51 @@ angular.module('app.controllers', [])
 	$ionicNavBarDelegate.showBackButton(false);
 })
 
-.controller('addPlayersCtrl', function($scope, $ionicNavBarDelegate) {
+.controller('addPlayersCtrl', function($scope, $ionicNavBarDelegate, services) {
 	$ionicNavBarDelegate.showBackButton(false);
+	$scope.users = services;
 })
 
-.controller('signupCtrl', function($scope, $ionicNavBarDelegate) {
+.controller('signupCtrl', function($scope, $ionicNavBarDelegate, services) {
 	$ionicNavBarDelegate.showBackButton(false);
+	$scope.users = services;
 })
 
-.controller('loginCtrl', function($scope, $ionicNavBarDelegate) {
+.controller('loginCtrl', function($scope, $ionicNavBarDelegate, services) {
 	$ionicNavBarDelegate.showBackButton(false);
+	$scope.users = services;
 })
 
 .controller('statsCtrl', function($scope, $ionicNavBarDelegate, services) {
 	$ionicNavBarDelegate.showBackButton(false);
-	$scope.user = services.getUser();
+	$scope.users = services;
 })
 
-.controller('pendingCtrl', function($scope, $ionicNavBarDelegate) {
+.controller('pendingCtrl', function($scope, $ionicNavBarDelegate, services) {
 	$ionicNavBarDelegate.showBackButton(false);
+	$scope.users = services;
 })
 
-.controller('gameNightCtrl', function($scope, $ionicNavBarDelegate) {
+.controller('gameNightCtrl', function($scope, $ionicNavBarDelegate, services) {
 	$ionicNavBarDelegate.showBackButton(false);
+	$scope.users = services;
 })
 
-.controller('gameDayCtrl', function($scope, $ionicNavBarDelegate) {
+.controller('gameDayCtrl', function($scope, $ionicNavBarDelegate, services) {
 	$ionicNavBarDelegate.showBackButton(false);
+	$scope.users = services;
 })
 
-.controller('voteCtrl', function($scope, $ionicNavBarDelegate) {
+.controller('voteCtrl', function($scope, $ionicNavBarDelegate, services) {
 	$ionicNavBarDelegate.showBackButton(false);
+	$scope.users = services;
 })
 
 .controller('rulesCtrl', function($scope, $ionicNavBarDelegate) {
 	$ionicNavBarDelegate.showBackButton(false);
 })
 
-.controller('resultsCtrl', function($scope, $ionicNavBarDelegate) {
+.controller('resultsCtrl', function($scope, $ionicNavBarDelegate, services) {
 	$ionicNavBarDelegate.showBackButton(false);
+	$scope.users = services;
 })
--- mafiaApp/www/js/services.js
+++ mafiaApp/www/js/services.js
@@ -1,18 +1,29 @@
 angular.module('app.services', [])
 
 .factory('services', [function(){
-	var user={};
-		user.games = 210;
-		user.victories = 5;
+	var users={};
+		users.uname = "";
+		users.plyr2 = "";
+		users.plyr3 = "";
+		users.plyr4 = "";
+		users.plyr5 = "";
+		users.plyr6 = "";
+		users.plyr7 = "";
+
+		users.role1 = "Mafia";
+		users.role2 = "Cop";
+		users.role3 = "Vanilla";
+		users.role4 = "Mafia";
+		users.role5 = "Medic";
+		users.role6 = "Vanilla";
+		users.role7 = "Vanilla";
+
+		users.voted = "voted";
+
+		users.games = 10;
+		users.victories = 5;
+		users.mafiaWin = 3;
+		users.townWin = 2;
 	
-	return{
-		getUser: function(){
-			return user;
-		},
-	}
-}])
-
-// .service('BlankService', [function(){
-
-// }]);
-
+	return users;
+}]);
